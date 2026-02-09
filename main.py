26737aaf5cb1845b3ef00c1bb6b7bd94032966af
Ferranarna <ferran.arnaus.garcia23@gmail.com>
2026-02-09 21:43:11 +0100

    fix: CORS main.py

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from src.infrastructure.database import engine, Base
 from src.infrastructure.adapters.api.routers import project_router, epic_router, task_router
 
@@ -9,6 +10,21 @@ app = FastAPI(
     title="Kanban API",
     description="API para gestionar Proyectos, Épicas y Tareas",
     version="1.0.0"
+)
+
+# --- 2. CONFIGURACIÓN DE CORS ---
+# Aquí definimos quién tiene permiso para llamar a la API
+origins = [
+    "http://localhost:5173",    # Puerto por defecto de Vite (React)
+    "http://127.0.0.1:5173",   # A veces el navegador usa la IP en lugar de localhost
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,      # Lista de invitados
+    allow_credentials=True,
+    allow_methods=["*"],        # Permitir todos los métodos (GET, POST, PUT, DELETE)
+    allow_headers=["*"],        # Permitir todas las cabeceras
 )
 
 # 2. Registrar los routers
